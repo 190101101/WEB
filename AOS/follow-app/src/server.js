48cf2ebcb9b4fca1e57a5a9d3e761e58f2fd2882
190101101 <190101101@mail.ru>
2024-01-19 07:58:18 +0400

    15 socket-io\162 create server

--- AOS/follow-app/src/server.js
+++ AOS/follow-app/src/server.js
@@ -1,23 +1,20 @@
 require("dotenv").config();
-const path = require('path');
+const path = require("path");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
-// app.use(logger("dev"));
+const PORT = process.env.PORT || 3000;
 
+// app.use(logger("dev"));
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 mongoose
   .connect(process.env.MONGO_DB)
-  .then((data) =>
-    app.listen(process.env.PORT, () => {
-      console.log(`http://localhost:${process.env.PORT}`);
-    })
-  )
+  .then((data) => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.render("home"));
