5af667ae5fc12b192c5b54fbda8d047704a8129e
190101101 <190101101@mail.ru>
2024-01-19 12:10:03 +0400

    17 follow-app\184 bcrypt

---
+++ base/17 follow-app/184 bcrypt/controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+
+const signup_get = (req, res) => {
+  res.render("signup");
+};
+
+const login_get = (req, res) => {
+  res.render("login");
+};
+
+const signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    console.log(user);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json(error.errors);
+  }
+};
+
+const login_post = async (req, res) => {
+  const { email, password } = req.body;
+  res.send("logined");
+};
+
+module.exports = {
+  signup_get,
+  login_get,
+  signup_post,
+  login_post,
+};
