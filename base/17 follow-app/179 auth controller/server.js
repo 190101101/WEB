9421e0148a8d0dbdc19683fd9e49dbc5a820217c
190101101 <190101101@mail.ru>
2024-01-19 10:24:30 +0400

    17 follow-app\179 auth controller

---
+++ base/17 follow-app/179 auth controller/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// app.use(logger("dev"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then((data) => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/works", (req, res) => res.render("works"));
