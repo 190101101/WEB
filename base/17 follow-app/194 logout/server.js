69b0977b7386ccac839bc34a15553fa299eff2de
190101101 <190101101@mail.ru>
2024-01-20 07:58:54 +0400

    17 follow-app\194 logout

---
+++ base/17 follow-app/194 logout/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoutes");
+const {authControl} = require("./middleware/AuthMiddleware");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then((data) =>
+    app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
+  )
+  .catch((err) => console.log(err));
+
+app.get("/", authControl, (req, res) => res.render("home"));
+app.get("/works", authControl, (req, res) => res.render("works"));
+
+app.use(authRoutes);
