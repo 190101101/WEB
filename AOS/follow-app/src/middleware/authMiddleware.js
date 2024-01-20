2ff407fed6e234fde2209833b61884068976efa7
190101101 <190101101@mail.ru>
2024-01-20 07:50:14 +0400

    17 follow-app\193 token control

---
+++ AOS/follow-app/src/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+
+const authControl = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if(token){
+        jwt.verify(token, process.env.JWT, (error, result) => {
+            if(error){
+                console.log(error.message);
+                res.redirect('/login')
+            }else{
+                console.log(result);
+                next();
+            }
+        });
+    }else{
+        res.redirect('/login')
+    }
+}
+
+module.exports = {authControl};
