77577e5b76466995a6e88ce78180da1c7a0a3406
190101101 <190101101@mail.ru>
2024-01-20 20:45:28 +0400

    19 react router\208 BrowserRouter switc route

---
+++ AOS/client/src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">home</a>
+      </li>
+      <li>
+        <a href="/article">article</a>
+      </li>
+      <li>
+        <a href="/category">category</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Nav;
