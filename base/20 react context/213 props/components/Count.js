9c233b33c0f9b35eb4ada0b742f9235a305897ed
190101101 <190101101@mail.ru>
2024-01-20 22:22:20 +0400

    20 react context\213 props

---
+++ base/20 react context/213 props/components/Count.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Count = ({userCount}) => {
+  return (
+    <div>count: {userCount}</div>
+  )
+}
+
+export default Count
---
+++ base/20 react context/213 props/components/User.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = ({ props }) => {
+  return <div>@{props.username}</div>;
+};
+
+export default User;
