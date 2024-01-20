33626f80e253166496d8c2d5da1232d00d3ac86b
190101101 <190101101@mail.ru>
2024-01-20 21:20:11 +0400

    19 react router\211 match

---
+++ base/19 react router/211 match/components/Nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Nav = () => {
+
+  const navStyle = {
+    color:'deeppink',
+    textDecoration:'none'
+  }
+
+  return (
+    <ul>
+      <li>
+        <Link style={navStyle} to="/">home</Link>
+      </li>
+      <li>
+        <Link style={navStyle} to="/article">article</Link>
+      </li>
+      <li>
+        <Link style={navStyle} to="/category">category</Link>
+      </li>
+      <li>
+        <Link style={navStyle} to="/users">users</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Nav;
