bb4ab2c1cd063eb88edfe28db31f2fc1f276204b
190101101 <190101101@mail.ru>
2024-01-22 20:11:11 +0400

    23 contact-app\249 delete

---
+++ base/23 contact-app/249 delete/components/contact/Table.js
@@ -0,0 +1,52 @@
+const Table = ({ props }) => {
+  return (
+    <table className="table table-dark">
+      <thead className="text-warning">
+        <tr>
+          <th>name</th>
+          <th>surname</th>
+          <th>email</th>
+          <th>phone</th>
+          <th>
+            <i className="fa fa-pencil"></i>
+          </th>
+          <th>
+            <i className="fa fa-trash-o"></i>
+          </th>
+        </tr>
+      </thead>
+      <tbody className="text-info">
+        {Object.keys(props.contactData).map((id) => (
+          <tr className="text-white" key={id}>
+            <td>{props.contactData[id].name}</td>
+            <td>{props.contactData[id].surname}</td>
+            <td>{props.contactData[id].email}</td>
+            <td>{props.contactData[id].phone}</td>
+            <td
+              onClick={() => {
+                props.setFormType(false);
+                props.setUpdateData({
+                  ...props.contactData[id],
+                  id: id,
+                });
+              }}
+              className="text-success"
+            >
+              <i className="fa fa-pencil"></i>
+            </td>
+            <td
+              onClick={() => {
+                props.deleteDataHandler(id);
+              }}
+              className="text-danger"
+            >
+              <i className="fa fa-trash-o"></i>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
---
+++ base/23 contact-app/249 delete/components/contact/Update.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Update = (props) => {
+
+  const inputRefs = {
+    name: useRef(null),
+    surname: useRef(null),
+    email: useRef(null),
+    phone: useRef(null),
+  };
+
+  useEffect(() => {
+    Object.keys(props.updateData).forEach(key => {
+      if (inputRefs[key] && inputRefs[key].current) {
+        inputRefs[key].current.value = props.updateData[key];
+      }
+    });
+  }, [props.updateData]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // get inputs key value data
+    const formData = Object.keys(inputRefs).reduce((values, key) => {
+      values[key] = inputRefs[key].current.value;
+      return values;
+    }, {});
+
+    // reset form
+    Object.keys(inputRefs).map((input) => {
+      inputRefs[input].current.value = "";
+    });
+
+    formData.id = props.updateData.id;
+    props.update(formData);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="">
+        <div className="input-group mb-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fa fa-user"></i>
+            </div>
+          </div>
+          <input
+            ref={inputRefs.name}
+            type="text"
+            name="name"
+            className="form-control bg-dark text-white"
+            placeholder="name"
+          />
+        </div>
+      </div>
+      <div className="">
+        <div className="input-group mb-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fa fa-user"></i>
+            </div>
+          </div>
+          <input
+            ref={inputRefs.surname}
+            type="text"
+            name="surname"
+            className="form-control bg-dark text-white"
+            placeholder="surname"
+          />
+        </div>
+      </div>
+
+      <div className="">
+        <div className="input-group mb-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fa fa-user"></i>
+            </div>
+          </div>
+          <input
+            ref={inputRefs.email}
+            type="text"
+            name="email"
+            className="form-control bg-dark text-white"
+            placeholder="email"
+          />
+        </div>
+      </div>
+
+      <div className="">
+        <div className="input-group mb-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fa fa-user"></i>
+            </div>
+          </div>
+          <input
+            ref={inputRefs.phone}
+            type="text"
+            name="phone"
+            className="form-control bg-dark text-white"
+            placeholder="phone"
+          />
+        </div>
+      </div>
+
+      <div className="">
+        <button
+          type="submit"
+          className="btn btn-block btn-outline-success mb-2"
+        >
+          update
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Update;
