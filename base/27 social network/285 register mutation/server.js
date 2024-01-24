8b0ed5d00c5427da86dc19a91729c8f43284bc10
190101101 <190101101@mail.ru>
2024-01-24 21:10:27 +0400

    27 social network\285 register mutation

---
+++ base/27 social network/285 register mutation/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then((res) => console.log("server connected to db"))
+  .catch((error) => console.log("something wrong"));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen({ port: 5000 }).then((response) => {
+  console.log(response.url);
+});
